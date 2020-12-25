9ea669803725927968640e14c56d3d446ecb35c3
arnaullfe <arnaullfe@gmail.com>
2020-12-25 23:14:35 +0100

    fent resource

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//List articles
+Route::get('articles','ArticleController@index');
+
+//List single article
+Route::get('articles/{id}','ArticleController@show');
+
+//create new article
+Route::post('articles','ArticleController@store');
+
+//update article
+Route::post('articles','ArticleController@store');
+
+//delete article
+Route::delete('articles','ArticleController@destroy');
